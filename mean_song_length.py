e91c787939355edc4de29044bb04c3d2ec9e4c1b
Droiline <annaruthgrace@hotmail.com>
2019-11-18 12:08:34 +0000

    Neatened code and tidied print statements

--- mean_song_length.py
+++ mean_song_length.py
@@ -3,43 +3,51 @@
 import requests
 import sys
 
+
 STATUS_CODE_OK = 200
+
+ARTIST_ENDPOINT = 'https://musicbrainz.org/ws/2/artist/'
+LYRIC_ENDPOINT = 'https://api.lyrics.ovh/v1/'
+
 
 def sanitise(query):
   return query.replace(' ', '_').lower()
 
+
 query = input('Enter artist name:\n')
 query = sanitise(query)
 
-try:
-  response = requests.get('https://musicbrainz.org/ws/2/artist/?query={}&fmt=json'.format(query))
-except requests.exceptions.ConnectionError as e:
-  print(e)
+artist_search = requests.get('{endpoint}?query={query}&fmt=json'.format(
+  endpoint=ARTIST_ENDPOINT,
+  query=query
+))
+
+if artist_search.status_code is not STATUS_CODE_OK:
+  print('Failed to access Musicbrainz server')
   sys.exit(1)
 
-if response.status_code is not STATUS_CODE_OK:
-  print('Failed to access server')
-  sys.exit(1)
+artist_search = artist_search.json()
+# for artist in artist_search['artists']:
+#   print('"{name}" {disamb}'.format(
+#     name=artist['name'],
+#     disamb=artist['disambiguation'] if 'disambiguation' in artist else ''
+#   ))
 
-response_json = response.json()
-print(len(response_json['artists']))
-for artist in response_json['artists']:
-  print('{id} - "{name}" {disamb}'.format(
-    id=artist['id'],
-    name=artist['name'],
-    disamb=artist['disambiguation'] if 'disambiguation' in artist else ''))
-
-artist = response_json['artists'][0]
-song_list = requests.get('https://musicbrainz.org/ws/2/artist/{artist_id}?inc=work-rels&fmt=json'.format(artist_id=artist['id']))
+artist = artist_search['artists'][0]
+song_list = requests.get('{endpoint}{artist_id}?inc=work-rels&fmt=json'.format(
+  endpoint=ARTIST_ENDPOINT,
+  artist_id=artist['id']
+))
 
 if song_list.status_code is not STATUS_CODE_OK:
-  print('Could not retrieve songs by {}'.format(artist))
+  print('Could not retrieve songs from Musicbrainz')
   sys.exit(1)
 
 song_list = song_list.json()
 word_counts = []
 for song in song_list['relations']:
-  lyrics = requests.get('https://api.lyrics.ovh/v1/{artist}/{song}'.format(
+  lyrics = requests.get('{endpoint}{artist}/{song}'.format(
+    endpoint=LYRIC_ENDPOINT,
     artist=sanitise(artist['name']),
     song=sanitise(song['work']['title'])
   ))
@@ -50,4 +58,4 @@ for song in song_list['relations']:
   word_counts.append(len(lyrics.json()['lyrics'].split()))
 
 mean = sum(word_counts) / len(word_counts)
-print('mean: {}'.format(mean))
+print('\nmean: {0:.2f}'.format(mean))
